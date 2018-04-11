0644c916da08e12a409cd5dab701c0c9b0f78ec4
Daniel Blackey <dblackey@sas.upenn.edu>
2018-04-11 10:20:23 -0400

    Critical 2

--- data_includes/example_data.js
+++ data_includes/example_data.js
@@ -38,24 +38,30 @@ var items = [
                           "She knew it was going to be a long day, so she packed a little "+
                           "extra in her lunch. By 3:00, she had had another two cups of coffee. "+
                           "She was exhausted by the time she made it home.</p>"+
-                          "<ol><li><p>How many cups of coffee did Jess have today?</p>"+
-                          "<input name='answer' type='text' class='obligatory'></li></ol>"}
+                          "<ol><li><p>How many cups of coffee did Jess have today? (Please enter a number)</p>"+
+                          "<input name='answer' type='number' class='obligatory'></li></ol>"}
     ],
     ["init",
         "Message", {html: "<p>Let's begin!</p>",
                     transfer: "click"}
     ],
     ["critical1",  // condition 1
-        "Form", {html: "<p style='width:500px'>John met three coworkers. He later went to the park with his kids. Before heading home, "+
+        "Form", {html: "<p style='width:500px'>John met three coworkers yesterday. He later went to the park with his kids. Before heading home, "+
                           "he also met a good friend of his.</p>"+
-                          "<ol><li><p>How many meetings did John have?</p>"+
-                          "<input name='answer' type='text' class='obligatory'></li></ol>"}
+                          "<ol><li><p>How many meetings did John have? (Please enter a number)</p>"+
+                          "<input name='answer' type='number' class='obligatory'></li></ol>"}
+    ],
+    ["critical2",
+        "Form", {html: "<p style='width:500px'>Dave went for a walk in the park. He also ran into four friends of his. Later that day, he "+
+                          "called them to schedule another time to catch up. He also ran into his teacher, Beth, on his way home.</p>"+
+                          "<ol><li><p>How many times did Dave run into friends? (Please enter a number)</p>"+
+                          "<input name='answer' type='number' class='obligatory'></li></ol>"}
     ],
     [ "filler",
         "Form", {html: "<p style='width:500px'>Jonathan ate three pizzas last week. He also drank a glass of OJ, "+
-                          "and ate a piece of cake.. </p>"+
-                          "<ol><li><p>How many times did Jonathan eat??</p>"+
-                          "<input name='answer' type='text' class='obligatory'></li></ol>"}
+                          "and ate a piece of cake.</p>"+
+                          "<ol><li><p>How many times did Jonathan eat? (Please enter a number)</p>"+
+                          "<input name='answer' type='number' class='obligatory'></li></ol>"}
     ]
     
 ];
