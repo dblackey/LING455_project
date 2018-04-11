220615fc5b75090de0cdb0e55b68821aeb7b08d6
Daniel Blackey <dblackey@sas.upenn.edu>
2018-04-11 10:28:27 -0400

    changed to text'

--- data_includes/example_data.js
+++ data_includes/example_data.js
@@ -39,7 +39,7 @@ var items = [
                           "extra in her lunch. By 3:00, she had had another two cups of coffee. "+
                           "She was exhausted by the time she made it home.</p>"+
                           "<ol><li><p>How many cups of coffee did Jess have today? (Please enter a number)</p>"+
-                          "<input name='answer' type='number' class='obligatory'></li></ol>"}
+                          "<input name='answer' type='text' class='obligatory'></li></ol>"}
     ],
     ["init",
         "Message", {html: "<p>Let's begin!</p>",
@@ -49,26 +49,26 @@ var items = [
         "Form", {html: "<p style='width:500px'>John met three coworkers yesterday. He later went to the park with his kids. Before heading home, "+
                           "he also met a good friend of his.</p>"+
                           "<ol><li><p>How many meetings did John have? (Please enter a number)</p>"+
-                          "<input name='answer' type='number' class='obligatory'></li></ol>"}
+                          "<input name='answer' type='text' class='obligatory'></li></ol>"}
     ],
     ["critical2",
         "Form", {html: "<p style='width:500px'>Dave went for a walk in the park. He also ran into four friends of his. Later that day, he "+
                           "called them to schedule another time to catch up. He also ran into his teacher, Beth, on his way home.</p>"+
                           "<ol><li><p>How many times did Dave run into friends? (Please enter a number)</p>"+
-                          "<input name='answer' type='number' class='obligatory'></li></ol>"}
+                          "<input name='answer' type='text' class='obligatory'></li></ol>"}
     ],
     ["critical3",
         "Form", {html: "<p style='width:500px'>Mary was going out for a jog when she heard an alarm go off. In all the chaos, she bumped into three kids who couldn’t "+
                 "find their parents. As it turned out, someone had accidently set off the tornado alert. Mary was exhausted by the time she made it home. "+
                   "She bumped into her friend Alison on the way, but told her she couldn’t chat.</p>"+
                   "<ol><li><p>How many times did Mary bump into people? (Please enter a number)</p>"+
-                  "<input name='answer' type='number' class='obligatory'></li></ol>"}
+                  "<input name='answer' type='text' class='obligatory'></li></ol>"}
     ],
     [ "filler",
         "Form", {html: "<p style='width:500px'>Jonathan ate three pizzas last week. He also drank a glass of OJ, "+
                           "and ate a piece of cake.</p>"+
                           "<ol><li><p>How many times did Jonathan eat? (Please enter a number)</p>"+
-                          "<input name='answer' type='number' class='obligatory'></li></ol>"}
+                          "<input name='answer' type='text' class='obligatory'></li></ol>"}
     ]
     
 ];
